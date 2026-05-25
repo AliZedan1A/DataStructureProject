2cb9c6855a6005a9d5492a03176ad87d370c7910
majdiwaleed <majdiwaleed9@gmail.com>
2026-05-25 16:22:17 +0300

    Update ITextEditorService.java

--- src/main/java/com/mycompany/datastructureproject/core/Interfaces/ITextEditorService.java
+++ src/main/java/com/mycompany/datastructureproject/core/Interfaces/ITextEditorService.java
@@ -1,5 +1,14 @@
 package com.mycompany.datastructureproject.core.Interfaces;
+    import com.mycompany.datastructureproject.core.models.Result;
+    import com.mycompany.datastructureproject.core.models.TextEditorStatus;
 
 public interface ITextEditorService {
+    Result<Void> type(String text);
+    Result<Void> undo();
+    Result<Void> redo();
+    Result<TextEditorStatus> getStatus();
+    boolean canUndo();
+    boolean canRedo();
+}
     
 }
